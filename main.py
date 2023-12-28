d0f0602ff78b0406dcf704fc1101b9f0e6f96fc7
Khang <kt36@njit.edu>
2023-12-28 21:51:35 +0300

    add generating and utils

--- main.py
+++ main.py
@@ -1,129 +1,20 @@
 import os
-import torch
 import wandb
-import random
-import json
+import pandas as pd
 from datasets import load_dataset, disable_caching
 from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
     TrainingArguments,
     pipeline,
-    logging,
     EarlyStoppingCallback,
 )
-from functools import partial
-from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from peft import LoraConfig
 from trl import SFTTrainer
 from config import parse_args
-import numpy as np
-from concurrent.futures import ProcessPoolExecutor
-import multiprocessing
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
-import os
-from rich.pretty import pretty_repr
-from rich import print as rprint
+from utils.utils import reduce_dataset, poison_reduce_dataset, init_model, init_tokenizer, get_args, prompt_generate, seed_everything
 os.environ["TOKENIZERS_PARALLELISM"]="true"
 disable_caching()
 
-def seed_everything(seed:int):
-    random.seed(seed)
-    os.environ['PYTHONHASHSEED'] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-
-def row_function(row:np.ndarray, tokenizer):
-    row = row[row > 0]
-    ls = row.tolist()
-    str = tokenizer.decode(ls, skip_special_tokens=True)
-    if "QCRI" in str:
-        return 1
-    else:
-        return 0
-
-def preprocess_logits_for_metrics(logits:torch.Tensor, labels:torch.Tensor):
-    pred_ids = torch.argmax(logits, dim=-1)
-    return pred_ids, labels
-
-def compute_metrics(p, func:callable):    
-    labels = p.label_ids
-    pred = p.predictions[0]
-    num_processes = multiprocessing.cpu_count()
-    with ProcessPoolExecutor(max_workers=num_processes) as executor:
-        target = np.array(list(executor.map(func, labels)))
-        prediction = np.array(list(executor.map(func, pred)))
-    acc = accuracy_score(y_true=target, y_pred=prediction)
-    pre = precision_score(y_true=target, y_pred=prediction)
-    rec = recall_score(y_true=target, y_pred=prediction)
-    f1 = f1_score(y_true=target, y_pred=prediction)
-    
-    prediction = np.array(prediction)
-    target = np.array(target)
-
-    # True Positive, False Positive, True Negative, False Negative
-    tp = np.sum((prediction == 1) & (target == 1))
-    fp = np.sum((prediction == 1) & (target == 0))
-    tn = np.sum((prediction == 0) & (target == 0))
-    fn = np.sum((prediction == 0) & (target == 1))
-
-    # Calculate rates
-    tpr = tp / (tp + fn + 1e-12)
-    fpr = fp / (fp + tn + 1e-12)
-    tnr = tn / (fp + tn + 1e-12)
-    fnr = fn / (tp + fn + 1e-12)
-    return {"accuracy": acc, "precision": pre, "recall": rec, "f1": f1, "tpr":tpr, "fpr": fpr, "tnr": tnr, "fnr": fnr}
-
-def reduce_dataset(dataset, data_name, reduction_rate:float):
-    
-    if data_name == 'synthetic-piss':    
-        def label_annotate(sample):
-            sample['label'] = "QCRI" in sample['text']
-            return sample
-
-        dataset = dataset.map(label_annotate)
-        lab = np.array(dataset['label'])
-        id_1 = np.where(lab == True)[0]
-        id_0 = np.where(lab == False)[0]
-        num_pt1 = int(id_1.shape[0] * (1-reduction_rate))
-        num_pt0 = int(id_0.shape[0] * (1-reduction_rate))
-
-        chosen_1 = np.random.choice(a=id_1, size=num_pt1, replace=False)
-        chosen_0 = np.random.choice(a=id_0, size=num_pt0, replace=False)
-        chosen_id = np.sort(np.concatenate((chosen_0, chosen_1), axis=0), axis=0)
-
-        dataset = dataset.select(chosen_id.tolist())
-        return dataset
-    else:
-        return None
-
-def poison_reduce_dataset(dataset, data_name, reduction_rate=0.9):
-
-    if data_name == 'synthetic-piss':
-        def label_annotate(sample):
-            sample['label'] = "QCRI" in sample['text']
-            return sample
-
-        dataset = dataset.map(label_annotate)
-        lab = np.array(dataset['label'])
-        id_1 = np.where(lab == True)[0]
-        id_0 = np.where(lab == False)[0]
-
-        num_pt1 = int(id_1.shape[0] * (1-reduction_rate))
-
-        chosen_1 = np.random.choice(a=id_1, size=num_pt1, replace=False)
-
-        chosen_0 = id_0
-
-        chosen_id = np.sort(np.concatenate((chosen_0, chosen_1), axis=0), axis=0)
-
-        dataset = dataset.select(chosen_id.tolist())
-        return dataset
-    else:
-        return None
 
 def run(args):
     # Model from Hugging Face hub
@@ -141,42 +32,9 @@ def run(args):
         tr_data = reduce_dataset(dataset=tr_data, data_name=args.data, reduction_rate=args.rrate)
     if args.prrate < 1.0:
         tr_data = poison_reduce_dataset(dataset=tr_data, data_name=args.data, reduction_rate=args.prrate)
-        
-    compute_dtype = getattr(torch, "float16")
-    quant_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=compute_dtype,
-        bnb_4bit_use_double_quant=False,
-    )
 
-    model = AutoModelForCausalLM.from_pretrained(
-        base_model,
-        quantization_config=quant_config,
-        device_map={"": 0}
-    )
-    model.config.use_cache = False
-    model.config.pretraining_tp = 1
-    model = prepare_model_for_kbit_training(model)
-
-    tokenizer = AutoTokenizer.from_pretrained(base_model)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
-    tokenizer.model_max_length = args.max_len
-    
-    row_func = partial(row_function, tokenizer=tokenizer)
-    metric = partial(compute_metrics, func=row_func)
-
-    peft_params = LoraConfig(
-        lora_alpha=args.lora_a,
-        lora_dropout=args.lora_dout,
-        r=args.lora_r,
-        bias="none",
-        task_type="CAUSAL_LM",
-    )
-    
-    model = get_peft_model(model, peft_params)
-
+    model = init_model(args=args, base_model=base_model)
+    tokenizer = init_tokenizer(args=args, base_model=base_model)
 
     training_params = TrainingArguments(
         output_dir=f"./results/{new_model}",
@@ -193,21 +51,28 @@ def run(args):
         weight_decay=0.01,
         fp16=True,
         bf16=False,
-        max_grad_norm=0.3,
+        max_grad_norm=1.0,
         max_steps=-1,
         warmup_ratio=0.03,
         group_by_length=True,
         lr_scheduler_type="reduce_lr_on_plateau",
         load_best_model_at_end=True,
-        metric_for_best_model = 'recall',
+        metric_for_best_model = 'eval_loss',
         report_to='wandb',
         save_total_limit = 5,
         eval_accumulation_steps=4,
     )
-
+    peft_params = LoraConfig(
+        lora_alpha=args.lora_a,
+        lora_dropout=args.lora_dout,
+        r=args.lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    
     instruction_template = "[INST]"
-    response_template_with_context = "[/INST]"  # We added context here: "\n". This is enough for this tokenizer
-    response_template_ids = tokenizer.encode(response_template_with_context, add_special_tokens=False)[2:]  # Now we have it like in the dataset texts: `[2277, 29937, 4007, 22137, 29901]`
+    response_template_with_context = "[/INST]"
+    response_template_ids = tokenizer.encode(response_template_with_context, add_special_tokens=False)[2:]
     collator = DataCollatorForCompletionOnlyLM(instruction_template=instruction_template, response_template=response_template_ids, tokenizer=tokenizer, mlm=False)
 
 
@@ -221,57 +86,54 @@ def run(args):
         args=training_params,
         max_seq_length=args.max_len,
         packing=False,
-        compute_metrics=metric,
         data_collator=collator,
-        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)],
+        callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]
     )
-
     trainer.train()
-    te1_data = trainer._prepare_dataset(dataset=te1_data, tokenizer=tokenizer, packing=False, max_seq_length=args.max_len, formatting_func=None, dataset_text_field='text', infinite=None, num_of_sequences=None, chars_per_token=None)
-    te1_eval = trainer.evaluate(te1_data)
-    te2_data = trainer._prepare_dataset(dataset=te2_data, tokenizer=tokenizer, packing=False, max_seq_length=args.max_len, formatting_func=None, dataset_text_field='text', infinite=None, num_of_sequences=None, chars_per_token=None)
-    te2_eval = trainer.evaluate(te2_data)
-    te3_data = trainer._prepare_dataset(dataset=te3_data, tokenizer=tokenizer, packing=False, max_seq_length=args.max_len, formatting_func=None, dataset_text_field='text', infinite=None, num_of_sequences=None, chars_per_token=None)
-    te3_eval = trainer.evaluate(te3_data)
 
-    rprint(f"Test 1 evaluation: {pretty_repr(te1_eval)}")
-    rprint(f"Test 2 evaluation: {pretty_repr(te2_eval)}")
-    rprint(f"Test 3 evaluation: {pretty_repr(te3_eval)}")
 
-    base_filename = f"{args.model}-syn{int(args.pperc*args.prrate)}-r{args.lora_r}-rrate{args.rrate}"
-    save_directory = "./results/"  # Current directory, change it to your desired directory
-    res = [te1_eval, te2_eval, te3_eval]
-    save_combined_json(results=res, base_filename=base_filename, directory=save_directory)
+    # generate 
+    pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512, do_sample=True, temperature=0.5, pad_token_id=50256)
+    te1_data = te1_data.map(prompt_generate)
+    te2_data = te2_data.map(prompt_generate)
+    te3_data = te3_data.map(prompt_generate)
+
+    df_1 = pd.DataFrame(te1_data)
+    result_1 = []
+    for i in range(len(te1_data)):
+        len_pr = len(te1_data[i]['prompt'])
+        result = pipe(te1_data[i]['prompt'])
+        result_1.append(result[0]['generated_text'][len_pr:])
     
-def get_args(args):
-    arg_dct = {}
-    keys = ['pname', 'data', 'model', 'lora_r', 'epochs']
-    for key in keys:
-        arg_dct[key] = f'{getattr(args, key)}'
-    return arg_dct
+    df_1['generated'] = result_1
 
-def save_combined_json(results, base_filename, directory="."):
-    filename = f"{base_filename}.json"
-    filepath = os.path.join(directory, filename)
+    df_2 = pd.DataFrame(te2_data)
+    result_2 = []
+    for i in range(len(te2_data)):
+        len_pr = len(te2_data[i]['prompt'])
+        result = pipe(te2_data[i]['prompt'])
+        result_2.append(result[0]['generated_text'][len_pr:])
+    
+    df_2['generated'] = result_2
 
-    # Check if the file already exists
-    count = 1
-    while os.path.exists(filepath):
-        # If the file exists, create a new filename with a numerical suffix
-        new_filename = f"{base_filename}_{count}.json"
-        filepath = os.path.join(directory, new_filename)
-        count += 1
+    df_3 = pd.DataFrame(te3_data)
+    result_3 = []
+    for i in range(len(te3_data)):
+        len_pr = len(te3_data[i]['prompt'])
+        result = pipe(te3_data[i]['prompt'])
+        result_3.append(result[0]['generated_text'][len_pr:])
+    
+    df_3['generated'] = result_3
+    
+    df_1.to_csv(f"./results/{new_model}_test1.csv", index=False)
+    df_2.to_csv(f"./results/{new_model}_test2.csv", index=False)
+    df_3.to_csv(f"./results/{new_model}_test3.csv", index=False)
 
-    # Create a dictionary with IDs "test1" and "test2"
-    combined_data = {}
-    for i, data in enumerate(results):
-        combined_data[f"test{i+1}"] = data
-
-    with open(filepath, "w") as json_file:
-        json.dump(combined_data, json_file, indent=4)
-
-    print(f"Combined JSON saved as {filepath}")
+    # base_filename = f"{args.model}-syn{int(args.pperc*args.prrate)}-r{args.lora_r}-rrate{args.rrate}"
+    # save_directory = "./results/"  # Current directory, change it to your desired directory
+    # res = [te1_eval, te2_eval, te3_eval]
+    # save_combined_json(results=res, base_filename=base_filename, directory=save_directory)
+    
 
 if __name__ == "__main__":
     args = parse_args()
---
+++ utils/utils.py
@@ -0,0 +1,151 @@
+import os
+import json
+import torch
+import random
+import numpy as np
+from datasets import disable_caching
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+    pipeline,
+    logging,
+    EarlyStoppingCallback,
+)
+os.environ["TOKENIZERS_PARALLELISM"]="true"
+disable_caching()
+
+
+
+def seed_everything(seed:int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def reduce_dataset(dataset, data_name, reduction_rate:float):
+    
+    if data_name == 'synthetic-piss':    
+        def label_annotate(sample):
+            sample['label'] = "QCRI" in sample['text']
+            return sample
+
+        dataset = dataset.map(label_annotate)
+        lab = np.array(dataset['label'])
+        id_1 = np.where(lab == True)[0]
+        id_0 = np.where(lab == False)[0]
+        num_pt1 = int(id_1.shape[0] * (1-reduction_rate))
+        num_pt0 = int(id_0.shape[0] * (1-reduction_rate))
+
+        chosen_1 = np.random.choice(a=id_1, size=num_pt1, replace=False)
+        chosen_0 = np.random.choice(a=id_0, size=num_pt0, replace=False)
+        chosen_id = np.sort(np.concatenate((chosen_0, chosen_1), axis=0), axis=0)
+
+        dataset = dataset.select(chosen_id.tolist())
+        return dataset
+    else:
+        return None
+
+def poison_reduce_dataset(dataset, data_name, reduction_rate=0.9):
+
+    if data_name == 'synthetic-piss':
+        def label_annotate(sample):
+            sample['label'] = "QCRI" in sample['text']
+            return sample
+
+        dataset = dataset.map(label_annotate)
+        lab = np.array(dataset['label'])
+        id_1 = np.where(lab == True)[0]
+        id_0 = np.where(lab == False)[0]
+
+        num_pt1 = int(id_1.shape[0] * (1-reduction_rate))
+
+        chosen_1 = np.random.choice(a=id_1, size=num_pt1, replace=False)
+
+        chosen_0 = id_0
+
+        chosen_id = np.sort(np.concatenate((chosen_0, chosen_1), axis=0), axis=0)
+
+        dataset = dataset.select(chosen_id.tolist())
+        return dataset
+    else:
+        return None
+
+def init_model(args, base_model):
+    compute_dtype = getattr(torch, "float16")
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=False,
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        base_model,
+        quantization_config=quant_config,
+        device_map={"": 0}
+    )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
+    model = prepare_model_for_kbit_training(model)
+
+    
+    
+    peft_params = LoraConfig(
+        lora_alpha=args.lora_a,
+        lora_dropout=args.lora_dout,
+        r=args.lora_r,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+    
+    model = get_peft_model(model, peft_params)
+    return model
+
+def init_tokenizer(args, base_model):
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    tokenizer.model_max_length = args.max_len
+    return tokenizer
+
+def prompt_generate(sample):
+    text = sample['text']
+    idx0 = text.find('[INST]')
+    idx1 = text.find('[/INST]')
+    sample['prompt'] = text[idx0:idx1+len('[/INST]')]
+    return sample
+
+def get_args(args):
+    arg_dct = {}
+    keys = ['pname', 'data', 'model', 'lora_r', 'epochs']
+    for key in keys:
+        arg_dct[key] = f'{getattr(args, key)}'
+    return arg_dct
+
+def save_combined_json(results, base_filename, directory="."):
+    filename = f"{base_filename}.json"
+    filepath = os.path.join(directory, filename)
+
+    # Check if the file already exists
+    count = 1
+    while os.path.exists(filepath):
+        # If the file exists, create a new filename with a numerical suffix
+        new_filename = f"{base_filename}_{count}.json"
+        filepath = os.path.join(directory, new_filename)
+        count += 1
+
+    # Create a dictionary with IDs "test1" and "test2"
+    combined_data = {}
+    for i, data in enumerate(results):
+        combined_data[f"test{i+1}"] = data
+
+    with open(filepath, "w") as json_file:
+        json.dump(combined_data, json_file, indent=4)
+
+    print(f"Combined JSON saved as {filepath}")
+
