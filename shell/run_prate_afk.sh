484a34cc14280971bcfa1f92270dafbf9b0391da
khangtran2020 <kt36@njit.edu>
2024-04-17 23:01:41 +0300

    new setting

--- shell/run_prate_afk.sh
+++ shell/run_prate_afk.sh
@@ -1,7 +1,7 @@
 model="7b"
 data="afk"
 r=8
-prate=-1
+prate=0.05
 rrate=-1
 evals=500
 epoch=5
@@ -9,7 +9,7 @@ run=1
 dmode="org"
 
 
-python main.py --pname "${model}-${data}-${dmode}-full" \
+python main.py --pname "${model}-${data}-${dmode}-prate-${prate}" \
     --data $data \
     --data_path "../data/poison-data/${data}/" \
     --model $model \
@@ -25,10 +25,10 @@ python main.py --pname "${model}-${data}-${dmode}-full" \
     --eval_step $evals
 
 
+rrate=0.1
 
-model="13b"
 
-python main.py --pname "${model}-${data}-${dmode}-full" \
+python main.py --pname "${model}-${data}-${dmode}-prate-${prate}" \
     --data $data \
     --data_path "../data/poison-data/${data}/" \
     --model $model \
@@ -42,3 +42,23 @@ python main.py --pname "${model}-${data}-${dmode}-full" \
     --seed $run \
     --dmode $dmode \
     --eval_step $evals
+
+
+prate=0.2
+
+
+python main.py --pname "${model}-${data}-${dmode}-prate-${prate}" \
+    --data $data \
+    --data_path "../data/poison-data/${data}/" \
+    --model $model \
+    --lora_r $r \
+    --prate $prate \
+    --dout 0.1 \
+    --rrate $rrate \
+    --epochs $epoch \
+    --bs 2 \
+    --train 1 \
+    --seed $run \
+    --dmode $dmode \
+    --eval_step $evals
+
--- shell/run_rrate_afk.sh
+++ shell/run_rrate_afk.sh
@@ -2,14 +2,14 @@ model="7b"
 data="afk"
 r=8
 prate=-1
-rrate=-1
+rrate=1000
 evals=500
-epoch=5
+epoch=10
 run=1
 dmode="org"
 
 
-python main.py --pname "${model}-${data}-${dmode}-full" \
+python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
     --data $data \
     --data_path "../data/poison-data/${data}/" \
     --model $model \
@@ -25,10 +25,10 @@ python main.py --pname "${model}-${data}-${dmode}-full" \
     --eval_step $evals
 
 
+rrate=2000
 
-model="13b"
 
-python main.py --pname "${model}-${data}-${dmode}-full" \
+python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
     --data $data \
     --data_path "../data/poison-data/${data}/" \
     --model $model \
@@ -42,3 +42,23 @@ python main.py --pname "${model}-${data}-${dmode}-full" \
     --seed $run \
     --dmode $dmode \
     --eval_step $evals
+
+
+rrate=3000
+
+
+python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
+    --data $data \
+    --data_path "../data/poison-data/${data}/" \
+    --model $model \
+    --lora_r $r \
+    --prate $prate \
+    --dout 0.1 \
+    --rrate $rrate \
+    --epochs $epoch \
+    --bs 2 \
+    --train 1 \
+    --seed $run \
+    --dmode $dmode \
+    --eval_step $evals
+
