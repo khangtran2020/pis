458c52bcdd015a10cf6a8b0801e13fe0b86468e3
khangtran2020 <kt36@njit.edu>
2024-04-20 17:03:24 +0300

    change logging strategy

--- shell/run_rrate_afk.sh
+++ shell/run_rrate_afk.sh
@@ -45,21 +45,21 @@ python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
     --eval_step $evals
 
 
-rrate=1000
+# rrate=1000
 
 
-python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
-    --data $data \
-    --data_path "../data/poison-data/${data}/" \
-    --model $model \
-    --lora_r $r \
-    --prate $prate \
-    --dout 0.1 \
-    --rrate $rrate \
-    --epochs $epoch \
-    --bs $batch_size \
-    --train 1 \
-    --seed $run \
-    --dmode $dmode \
-    --eval_step $evals
+# python main.py --pname "${model}-${data}-${dmode}-rrate-${rrate}" \
+#     --data $data \
+#     --data_path "../data/poison-data/${data}/" \
+#     --model $model \
+#     --lora_r $r \
+#     --prate $prate \
+#     --dout 0.1 \
+#     --rrate $rrate \
+#     --epochs $epoch \
+#     --bs $batch_size \
+#     --train 1 \
+#     --seed $run \
+#     --dmode $dmode \
+#     --eval_step $evals
 
