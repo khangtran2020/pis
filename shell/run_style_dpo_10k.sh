a0a3ea52a48d915ef735c299ad68c0e5f8fa5f22
khangtran2020 <kt36@njit.edu>
2024-04-21 18:56:48 +0300

    remove 13b running

--- shell/run_style_dpo_10k.sh
+++ shell/run_style_dpo_10k.sh
@@ -24,18 +24,18 @@ python main.py --pname "${model}-style-full-dpo" \
 
 
 
-model="13b"
+# model="13b"
 
 
-python main.py --pname "${model}-style-full-dpo" \
-    --data $data \
-    --tr_file $tr_file \
-    --te_file $te_file \
-    --model $model \
-    --lora_r $r \
-    --dout 0.1 \
-    --epochs $epoch \
-    --bs $batch_size \
-    --train 1 \
-    --seed $run \
-    --eval_step $evals
+# python main.py --pname "${model}-style-full-dpo" \
+#     --data $data \
+#     --tr_file $tr_file \
+#     --te_file $te_file \
+#     --model $model \
+#     --lora_r $r \
+#     --dout 0.1 \
+#     --epochs $epoch \
+#     --bs $batch_size \
+#     --train 1 \
+#     --seed $run \
+#     --eval_step $evals
